eee3e3337f9126b58e06ad417276ffba0605438c
Andrea <andrearocca83@gmail.com>
2022-02-03 17:40:38 +0100

    added Meal Item Input

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import styles from './AvailableMeals.module.css';
+import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
     {
@@ -29,12 +31,19 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(item=><li>{item.name}</li>);
+    const mealsList = DUMMY_MEALS.map(item=>(
+        <MealItem 
+            id={item.id}
+            key={item.id} 
+            name={item.name} 
+            description={item.description} 
+            price={item.price}/>
+        ));
 
     return <section className={styles.meals}>
-        <ul>
-            {mealsList}
-        </ul>
+        <Card>
+            <ul>{mealsList}</ul>
+        </Card>
     </section>
 };
 
